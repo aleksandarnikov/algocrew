46e6f100bc151060e94af023be5b99c40d309326
Aleksandar Nikov <aleksandar.nikov@netcetera.com>
2020-05-04 23:46:15 +0200

    Solution

--- acm/2015/7181/main7181Alex.java
+++ acm/2015/7181/main7181Alex.java
@@ -5,26 +5,31 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Main7181Alex{
+public class Main7181Alex {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < N; i++) {
-            int n = sc.nextInt();
-            int z = 1;
-            if (map.containsKey(n)) {
-                z = map.get(n);
+        while (true) {
+            if (!sc.hasNext()) {
+                break;
             }
-            map.put(n, z);
-        }
-        for (Iterator iterator = map.entrySet().iterator(); iterator.hasNext();) {
-            Map.Entry<Integer, Integer> entry = (Map.Entry<Integer, Integer>) iterator.next();
-            if (entry.getValue() % K != 0) {
-                System.out.println(entry.getKey());
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+            for (int i = 0; i < N; i++) {
+                int n = sc.nextInt();
+                int z = 1;
+                if (map.containsKey(n)) {
+                    z = map.get(n);
+                }
+                map.put(n, z);
+            }
+            for (Iterator iterator = map.entrySet().iterator(); iterator.hasNext();) {
+                Map.Entry<Integer, Integer> entry = (Map.Entry<Integer, Integer>) iterator.next();
+                if (entry.getValue() % K != 0) {
+                    System.out.println(entry.getKey());
+                }
             }
         }
     }
